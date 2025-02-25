ef5bd95d845f2c5ca61113d003fe604a3b2467fb
Pedro Almeida <pedro.almeida@ventureslab.io>
2025-02-25 21:47:44 +0000

    feat: add axios back and point to fetch gist

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,84 +1,34 @@
-type RequestConfig = {
-  headers?: HeadersInit
-  body?: unknown
-} & Omit<RequestInit, 'body' | 'headers'>
+/*
+NextJS is famous for using its own `fetch` API under the hood, to control cache,
+revalidation, and other things that won't work with axios.
 
-export class HttpError extends Error {
-  constructor(
-    public response: Response,
-    public data: unknown,
-    message?: string,
-  ) {
-    super(message || `HTTP Error ${response.status}: ${response.statusText}`)
-    this.name = 'HttpError'
+However, for the sake of simplicity, we will use axios on this small poc.
+
+We could also use `fetch` API here, but it would require more code out-of-the-box.
+And my idea is to keep things as simple as possible for this howe work boilerplate.
+
+If you want to use `fetch` API, feel free to use this gist I've written as a starting point:
+https://gist.github.com/ppalmeida/9073ad42d0512d7590a313bc30350352
+*/
+import axios, { AxiosRequestConfig } from 'axios'
+
+const getBaseUrl = () => {
+  if (typeof window === 'undefined') {
+    return process.env.SERVER_API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL || '/'
   }
+
+  return process.env.NEXT_PUBLIC_API_BASE_URL
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || ''
-
-const parseResponse = async (response: Response) => {
-  const contentType = response.headers.get('content-type')
-  if (contentType?.includes('application/json')) {
-    return response.json()
-  }
-  if (contentType?.includes('text/')) {
-    return response.text()
-  }
-  return response.blob()
+const baseConfig = {
+  baseURL: getBaseUrl(),
 }
 
-const handleResponse = async (response: Response) => {
-  const data = await parseResponse(response)
+export const getHttpClient = (config?: AxiosRequestConfig) =>
+  axios.create({
+    ...baseConfig,
+    ...(config || {}),
+  })
 
-  if (!response.ok) {
-    throw new HttpError(response, data)
-  }
-
-  return data
-}
-
-const createUrl = (endpoint: string): string => (endpoint.startsWith('http') ? endpoint : `${BASE_URL}${endpoint}`)
-
-const request = async <T>(endpoint: string, config: RequestConfig = {}): Promise<T> => {
-  const { headers = {}, body, ...restConfig } = config
-
-  const requestConfig: RequestInit = {
-    ...restConfig,
-    headers: {
-      'Content-Type': 'application/json',
-      ...headers,
-    },
-  }
-
-  if (body) {
-    requestConfig.body = JSON.stringify(body)
-  }
-
-  const response = await fetch(createUrl(endpoint), requestConfig)
-  return handleResponse(response) as Promise<T>
-}
-
-export const get = <T>(endpoint: string, config: Omit<RequestConfig, 'body'> = {}) =>
-  request<T>(endpoint, { ...config, method: 'GET' })
-
-export const post = <T>(endpoint: string, data?: unknown, config: Omit<RequestConfig, 'body'> = {}) =>
-  request<T>(endpoint, { ...config, method: 'POST', body: data })
-
-export const put = <T>(endpoint: string, data?: unknown, config: Omit<RequestConfig, 'body'> = {}) =>
-  request<T>(endpoint, { ...config, method: 'PUT', body: data })
-
-export const patch = <T>(endpoint: string, data?: unknown, config: Omit<RequestConfig, 'body'> = {}) =>
-  request<T>(endpoint, { ...config, method: 'PATCH', body: data })
-
-export const del = <T>(endpoint: string, config: Omit<RequestConfig, 'body'> = {}) =>
-  request<T>(endpoint, { ...config, method: 'DELETE' })
-
-export const http = {
-  get,
-  post,
-  put,
-  patch,
-  delete: del,
-}
-
-export default http
+const httpClient = getHttpClient({})
+export default httpClient
